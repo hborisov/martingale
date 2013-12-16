5f555292127612f6b8f310ed7f00eabdf199922e
Hristo Borisov <ico.borisov@gmail.com>
2013-12-16 23:27:32 +0200

    I learned about object construction, call, bind, etc...

--- connector.js
+++ connector.js
@@ -5,25 +5,8 @@ var SELECT_MATCH_STATEMENT = "SELECT * FROM CSVData WHERE DIVISION = %1 AND MATC
 var INSERT_MATCH_STATEMENT = "INSERT INTO CSVData (DIVISION, MATCH_DATE, HOME_TEAM, AWAY_TEAM, FT_HOME_GOALS, FT_AWAY_GOALS, FT_RESULT) VALUES (%1, str_to_date(%2, '%d/%m/%Y'), %3, %4, %5, %6, %7)";
 var SELECT_ALL_MATCHES = "SELECT * FROM CSVData";
 
-var intermediateResult;
-var connection;
-
-function mysqlCallback(err, rows, fields) {
-	if (err) {
-		throw err;
-	}
-
-	return (rows.length > 0) ? true : false; 
-}
-
 function MysqlConnector(options) {
-	this.connectionOptions = {};
-	this.connectionOptions.host = options.host;
-	this.connectionOptions.user = options.user;
-	this.connectionOptions.password = options.password;
-	this.connectionOptions.database = options.database;
-
-	connection = mysql.createConnection(this.connectionOptions);
+	this.connection = mysql.createConnection(options);
 }
 
 MysqlConnector.prototype.checkIfMatchExists = function(matchData, cb) {
@@ -33,7 +16,7 @@ MysqlConnector.prototype.checkIfMatchExists = function(matchData, cb) {
 	checkMatchQuery.setParameter('3', matchData.HOME_TEAM);
 	checkMatchQuery.setParameter('4', matchData.AWAY_TEAM);
    
-    connection.query(checkMatchQuery.getQuery(), function (err, rows, fields) {
+    this.connection.query(checkMatchQuery.getQuery(), function (err, rows, fields) {
 		if (err) {
 			throw err;
 		}
@@ -52,17 +35,19 @@ MysqlConnector.prototype.insertNewMatch = function(matchData, cb) {
 	insertMatchQuery.setParameter('6', matchData.FT_AWAY_GOALS);
 	insertMatchQuery.setParameter('7', matchData.FT_RESULT);
 	
-	connection.query(insertMatchQuery.getQuery(), cb);
+	this.connection.query(insertMatchQuery.getQuery(), cb);
 };
 
 MysqlConnector.prototype.insertIfMatchNotExistsWithCallback = function(matchData, cb, cnt) {
+	var self = this;
+
 	var checkMatchQuery = require('./query')(SELECT_MATCH_STATEMENT);
 	checkMatchQuery.setParameter('1', matchData.DIVISION);
 	checkMatchQuery.setParameter('2', matchData.MATCH_DATE);
 	checkMatchQuery.setParameter('3', matchData.HOME_TEAM);
 	checkMatchQuery.setParameter('4', matchData.AWAY_TEAM);
 
-    connection.query(checkMatchQuery.getQuery(), function (err, rows, fields) {
+    this.connection.query(checkMatchQuery.getQuery(), function (err, rows, fields) {
 		if (err) {
 			throw err;
 		}
@@ -77,7 +62,7 @@ MysqlConnector.prototype.insertIfMatchNotExistsWithCallback = function(matchData
 			insertMatchQuery.setParameter('6', matchData.FT_AWAY_GOALS);
 			insertMatchQuery.setParameter('7', matchData.FT_RESULT);
 
-			connection.query(insertMatchQuery.getQuery(), function(err, rows, fields) {
+			self.connection.query(insertMatchQuery.getQuery(), function(err, rows, fields) {
 				cb(cnt);
 			});
 		} else {
@@ -88,7 +73,7 @@ MysqlConnector.prototype.insertIfMatchNotExistsWithCallback = function(matchData
 
 MysqlConnector.prototype.selectAllMatches = function(cb) {
 	var selectAllMatchesQuery = require('./query')(SELECT_ALL_MATCHES);
-	connection.query(selectAllMatchesQuery.getQuery(), function(err, rows, fields) {
+	this.connection.query(selectAllMatchesQuery.getQuery(), function(err, rows, fields) {
 		if(err) {
 			throw err;
 		}
--- martin_gale_strategy.js
+++ martin_gale_strategy.js
@@ -1,28 +1,37 @@
 var Connector = require('./connector');
-var cn = new Connector({
-	host: 'localhost',
-	user: 'root',
-	password: '@bcd!234',
-	database: 'martingale'
-});
-
-var matches = [];
-var constructed = {};
-constructed.flag = false;
 
 function MartinGaleStrategy() {
-	cn.selectAllMatches(function(rows) {
-		rows.forEach(function(element, index, array) {
-			matches.push(element);
-		});
-
-		constructed.flag = true;
-		console.log(matches.length);
+	this.matches = [];
+	this.constructed = {};
+	this.constructed.flag = false;
+	this.connection = new Connector({
+		host: 'localhost',
+		user: 'root',
+		password: '@bcd!234',
+		database: 'martingale'
 	});
 
-	console.log(matches.length);
-	console.log(constructed.flag);
+	this.connection.selectAllMatches((function(rows) {
+		rows.forEach((function(element, index, array) {
+			this.matches.push(element);
+		}).bind(this));
+
+		this.constructed.flag = true;
+		console.log('a ' + this.matches.length);
+	}).bind(this));
+
+	console.log(this.matches.length);
+	console.log(this.constructed.flag);
 
 }
+
+MartinGaleStrategy.prototype.getMatchesWithoutDraw = function() {
+
+};
+
+MartinGaleStrategy.prototype.len = function() {
+	console.log(this.matches.length);
+	console.log(this.constructed.flag);
+};
 
 module.exports = MartinGaleStrategy;
