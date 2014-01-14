dd13a53d19c12c30c0969c47fcd60cb1fc6c85ab
Hristo Borisov <ico.borisov@gmail.com>
2014-01-14 23:53:24 +0200

    file behind :)

---
+++ obsolete/bet365client_test.js
@@ -0,0 +1,16 @@
+var Bet365Client = require('./bet365client');
+
+if (process.argv[2] === undefined) {
+	console.log('Premier League: premier_league');
+	console.log('Second Bundes League: second_bundes_league');
+	console.log('Serie B: serie_b');
+	console.log('--------------');
+} else {
+	var league = process.argv[2];
+	var bc = new Bet365Client();
+	bc.resset(league);
+	bc.getOddsNew(function(odds) {
+		console.log(odds);
+	});
+}
+
